29baa1e412197275fe0e34da5a26202457a46dd7
j3lte <jwlagendijk@gmail.com>
2023-04-22 03:51:30 +0200

    Update export

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,3 @@
-export type { Artwork, Filter } from "./types.ts";
+export type { Artwork, ArtworkPlus, Filter } from "./types.ts";
 export { LanguageCode, Locale, Medium, Order, Size } from "./types.ts";
 export { WAMClient } from "./Client.ts";
